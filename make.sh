03ee60f5f41286e94d888f88e12977a0b422e5b5
dqzg12300 <512256984@qq.com>
2018-10-19 15:19:09 +0800

    改配置和添加make,还有启动脚本

---
+++ make.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+dir=$(cd `dirname $0`; pwd)
+echo $dir
+echo $1
+
+if [ ! $1 ]; then
+       cmd='help'
+else
+                cmd=$1
+fi
+
+if [ $cmd = "all" ];then
+		git submodule update --init
+
+        echo -e "\n make pbc"
+        cd $dir/pbc && pwd
+        make all
+        echo -e "\n make pbc binding"
+        cd $dir/pbc/binding/lua53 && pwd
+        make all
+
+        echo -e "\n make skynet"
+        cd $dir/skynet && pwd
+        make linux
+
+elif [ $cmd = "clean" ];then
+        echo -e "\n clean skynet"
+        cd $dir/skynet && pwd
+        make clean
+
+        echo -e "\n clean pbc"
+        cd $dir/pbc && pwd
+        make clean
+
+        echo -e "\n clean pbc binding"
+        cd $dir/pbc/binding/lua53 && pwd
+        make clean
+else
+        echo "make help"
+        echo "make.sh all"
+        echo "make.sh clean"
+fi
+
